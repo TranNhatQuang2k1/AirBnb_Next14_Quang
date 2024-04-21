222f1aacabb0d3798e9143638913236a25770f84
TranNhatQuang2k1 <nhatquangtran135@gmail.com>
2024-04-21 16:05:53 +0700

    first commit airbnb

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import { Box } from '@mui/material';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Box>hello</Box>
+    </main>
+  );
+}
